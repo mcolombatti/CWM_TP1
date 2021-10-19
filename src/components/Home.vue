ab185349532e0aeb4d3914f62c81f0015888097f
mcolombatti <maxicolombatti@gmail.com>
2021-10-18 22:35:56 -0300

    lunes noches

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,12 +5,15 @@
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-9">
                         <div class="text-center my-5">
-                            <h1 class="display-8 pb-5 fw-bolder text-white mb-2">Da Vinci Hosting, ofrecemos el mejor servicio segun tus necesidades</h1>
-                            <p class="lead text-white-50 mb-4">Necesitas un servicio de hosting web potente. Nuestras soluciones de hosting harán que tus sitios web sean realmente veloces.</p>
-                            <div class="d-grid pt-5 gap-3 d-sm-flex justify-content-sm-center"> 
-                            <li class="nav-item">
-                                <router-link class="  btn btn-primary btn-lg px-4 me-sm-3" to="/registro">Comenzar</router-link>
-                            </li>
+                            <h1 class="display-8 pb-5 fw-bolder text-white mb-2">Da Vinci Hosting, ofrecemos el mejor
+                                servicio segun tus necesidades</h1>
+                            <p class="lead text-white-50 mb-4">Necesitas un servicio de hosting web potente. Nuestras
+                                soluciones de hosting harán que tus sitios web sean realmente veloces.</p>
+                            <div class="d-grid pt-5 gap-3 d-sm-flex justify-content-sm-center">
+                                <li class="nav-item">
+                                    <router-link class="  btn btn-primary btn-lg px-4 me-sm-3" to="/registro">Comenzar
+                                    </router-link>
+                                </li>
                             </div>
                         </div>
                     </div>
@@ -18,42 +21,66 @@
             </div>
         </div>
         <!-- ACA DEBERIA TRAER LOS DATOS DE FIRESTORE--->
-        <p>{{prices}}</p> 
+        <p>{{prices}}</p>
         <section class="py-5 border-bottom" id="features">
             <div class="container px-5 my-5">
                 <div class="row gx-5">
                     <div class="col-lg-4 mb-5 mb-lg-0">
-                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                       <h2 class="text-h-meteorite-dark mt-15 mt-25-xxl mb-20 mb-30-xxl">Sitios web realmente rápidos</h2>
-                       <p class="text-h-gray paragraph fw-400">La velocidad de tu proyecto web es vital para que tu presencia en línea crezca. Sorprendé a tus posibles clientes con una velocidad increíble.</p>
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
+                                class="bi bi-collection"></i></div>
+                        <h2 class="text-h-meteorite-dark mt-15 mt-25-xxl mb-20 mb-30-xxl">Sitios web realmente rápidos
+                        </h2>
+                        <p class="text-h-gray paragraph fw-400">La velocidad de tu proyecto web es vital para que tu
+                            presencia en línea crezca. Sorprendé a tus posibles clientes con una velocidad increíble.
+                        </p>
                     </div>
                     <div class="col-lg-4 mb-5 mb-lg-0">
-                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                     <h2 class="text-h-meteorite-dark mt-15 mt-25-xxl mb-20 mb-30-xxl">Fácil de configurar</h2>
-                       <p class="text-h-gray paragraph fw-400">Hacé que tu sitio web esté online de forma fácil, solo deberás hacer un clic. Todo lo que buscas en un servicio de alojamiento, ofrecido de una forma clara.</p>
- 
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
+                                class="bi bi-building"></i></div>
+                        <h2 class="text-h-meteorite-dark mt-15 mt-25-xxl mb-20 mb-30-xxl">Fácil de configurar</h2>
+                        <p class="text-h-gray paragraph fw-400">Hacé que tu sitio web esté online de forma fácil, solo
+                            deberás hacer un clic. Todo lo que buscas en un servicio de alojamiento, ofrecido de una
+                            forma clara.</p>
+
                     </div>
                     <div class="col-lg-4">
-                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                   <h2 class="text-h-meteorite-dark mt-15 mt-25-xxl mb-20 mb-30-xxl">Para proyectos grandes o pequeños</h2>
-                       <p class="text-h-gray paragraph fw-400">Variedad de proyectos requiere variedad de tecnologías. Elegí el plan de alojamiento web que se ajuste a tus necesidades, después podrás mejorarlo a medida que tu negocio crezca.</p>
-  
+                        <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
+                                class="bi bi-toggles2"></i></div>
+                        <h2 class="text-h-meteorite-dark mt-15 mt-25-xxl mb-20 mb-30-xxl">Para proyectos grandes o
+                            pequeños</h2>
+                        <p class="text-h-gray paragraph fw-400">Variedad de proyectos requiere variedad de tecnologías.
+                            Elegí el plan de alojamiento web que se ajuste a tus necesidades, después podrás mejorarlo a
+                            medida que tu negocio crezca.</p>
+
                     </div>
                 </div>
             </div>
-        </section> 
-        <section> <div class="container px-5 my-5">
+        </section>
+        <section>
+            <div class="container px-5 my-5">
                 <div class="text-center mb-5">
                     <h2 class="fw-bolder">Ofrecemos los mejores Dominios a tu alcance</h2>
                     <p class="lead mb-0">Elegi el que mejor se adapte a tus necesidades</p>
-                </div></div><div class="mt-5 mb-5"><div class="container container-2020"><div id="domain-tlds"><div class="domain-tlds-container m-auto"><div class="d-flex align-items-center"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-com-mono-529ad71207.svg" alt="domain-com" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-online-mono-433cf47da4.svg" alt="domain-online" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-xyz-mono-9ce7cd9a61.svg" alt="domain-xyz" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-live-mono-abb1b65b94.svg" alt="domain-live" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-net-mono-dabca26189.svg" alt="domain-net" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-digital-mono-d41fb69ecc.svg" alt="domain-digital" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-me-mono-ed9e680028.svg" alt="domain-me" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"><img src="https://assets.hostinger.com/images/domain-checker-2020/tlds-all/icon-link-mono-4b485f1fe2.svg" alt="domain-link" class="domain-tld mr-5 ml-5 mr-0-xxl ml-0-xxl"></div></div></div></div></div></section>
+                </div>
+            </div>
+            <div class="mt-5 mb-5">
+                <div class="container container-2020">
+                    <div id="domain-tlds">
+                        <div class="domain-tlds-container m-auto">
+                            <div class="d-flex align-items-center"> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section class="bg-light py-5 border-bottom">
             <div class="container px-5 my-5">
                 <div class="text-center mb-5">
                     <h2 class="fw-bolder">Pagá según tu uso</h2>
-                    <p class="lead mb-0">Con nuestros planes económicos ya no tendrás que preocuparte por altos costos en tu servicio de Hosting</p>
+                    <p class="lead mb-0">Con nuestros planes económicos ya no tendrás que preocuparte por altos costos
+                        en tu servicio de Hosting</p>
                 </div>
-                <div class="row gx-5 justify-content-center"> 
+                <div class="row gx-5 justify-content-center">
                     <div class="col-lg-6 col-xl-4">
                         <div class="card mb-5 mb-xl-0">
                             <div class="card-body p-5">
@@ -98,7 +125,7 @@
                                 </ul>
                             </div>
                         </div>
-                    </div> 
+                    </div>
                     <div class="col-lg-6 col-xl-4">
                         <div class="card mb-5 mb-xl-0">
                             <div class="card-body p-5">
@@ -147,7 +174,7 @@
                                 </ul>
                             </div>
                         </div>
-                    </div> 
+                    </div>
                     <div class="col-lg-6 col-xl-4">
                         <div class="card">
                             <div class="card-body p-5">
@@ -196,7 +223,7 @@
                     </div>
                 </div>
             </div>
-        </section> 
+        </section>
         <section class="py-5 border-bottom">
             <div class="container px-5 my-5 px-5">
                 <div class="text-center mb-5">
@@ -204,25 +231,27 @@
                     <p class="lead mb-0">Nuestros clientes estan contentos de usar DaVinci Hosting</p>
                 </div>
                 <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-6"> 
+                    <div class="col-lg-6">
                         <div class="card mb-4">
                             <div class="card-body p-4">
                                 <div class="d-flex">
-                                    <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
+                                    <div class="flex-shrink-0"><i
+                                            class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
                                     <div class="ms-4">
                                         <p class="mb-1">La mejor velocidad y acceso en el mercado</p>
                                         <div class="small text-muted">- Mariana, Santiago de Chile</div>
                                     </div>
                                 </div>
                             </div>
-                        </div> 
+                        </div>
                         <div class="card">
                             <div class="card-body p-4">
                                 <div class="d-flex">
-                                    <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
+                                    <div class="flex-shrink-0"><i
+                                            class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
                                     <div class="ms-4">
                                         <p class="mb-1">
-                                        El mejor servicio de hosting lejos!</p>
+                                            El mejor servicio de hosting lejos!</p>
                                         <div class="small text-muted">- Maximiliano, Buenos Aires</div>
                                     </div>
                                 </div>
@@ -232,44 +261,53 @@
                 </div>
             </div>
         </section>
-        <div id="consola">             <div>{{prices}}</div>
-</div> 
-         
+        <div id="consola">
+            <div>{{prices}}</div>
+        </div>
+
     </div>
 </template>
 
-<script> 
-  
-import {getFirestore,  collection, getDocs } from "firebase/firestore";
-import 'babel-polyfill';
-import {getPrices} from '../services/firebase.js'
- 
-const db = getFirestore();
-export default {
-    name: "Home",
-    setup() {
-        // "ref" define una "ref"erencia reactiva, que se asocia al sistema de reactividad de Vue.
-        // Por ejemplo, si este valor cambia, cualquier vista que lo utiliza se va a actualizar también.
-        // Lo que hay que saber es que para usar luego los valores de una "ref", tenemos que accederlos
-        // siempre a través de su propiedad ".value" (pueden pensarlo como el reemplazo del "this." de la
-        // API de Options).
-        // Ese ".value" no aplica en la vista.
-        // const authUser = ref({
-        //     email: null,
-        //     displayName: null,
-        // });
-        //
-        // authStateSubscribe(user => {
-        //     // Actualizamos el "value" de la "ref" definida.
-        //     authUser.value = user;
-        // });
-        const {prices} = getPrices(db);
- console.log(getPrices(db))
+<script>
+    import {
+        getFirestore,
+        collection,
+        getDocs
+    } from "firebase/firestore";
+    import 'babel-polyfill';
+    import {
+        getPrices
+    } from '../services/firebase.js'
 
-        return { 
-            prices, 
-        };
-}}
+    const db = getFirestore();
+    export default {
+        name: "Home",
+        setup() {
+            // "ref" define una "ref"erencia reactiva, que se asocia al sistema de reactividad de Vue.
+            // Por ejemplo, si este valor cambia, cualquier vista que lo utiliza se va a actualizar también.
+            // Lo que hay que saber es que para usar luego los valores de una "ref", tenemos que accederlos
+            // siempre a través de su propiedad ".value" (pueden pensarlo como el reemplazo del "this." de la
+            // API de Options).
+            // Ese ".value" no aplica en la vista.
+            // const authUser = ref({
+            //     email: null,
+            //     displayName: null,
+            // });
+            //
+            // authStateSubscribe(user => {
+            //     // Actualizamos el "value" de la "ref" definida.
+            //     authUser.value = user;
+            // });
+            const {
+                prices
+            } = getPrices(db);
+            console.log(getPrices(db))
+
+            return {
+                prices,
+            };
+        }
+    }
 </script>
 
 <style>
